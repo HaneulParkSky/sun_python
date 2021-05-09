70f25342ca79146f6f69aff541f730f59a16971b
HaneulParkSky <45191981+HaneulParkSky@users.noreply.github.com>
2021-05-09 22:06:09 +0900

    Add files via upload

---
+++ backjoon_ex1.py
@@ -0,0 +1,61 @@
+
+
+# 1. 구구단
+
+#a = int(input("계산을 원하는 구구단 n 을 입력하세요:"))
+#for i in range(1,10):
+#    print(a,'*',i,"=",a*i)
+
+
+
+# 2. n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+
+
+#n = int(input(''))
+#sum = 0
+
+#for i in range(1, n+1):
+#    sum = sum + i
+
+#print(sum)
+
+
+# 3. 자연수 n이 주어졌을 때, 1부터 n까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+
+
+#n = int(input())
+#num = 1
+
+#for x in range(1,n+1):
+#    print(num)
+#    num = num + 1
+
+
+# 4. 자연수 n이 주어졌을 때, n부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+
+#n = int(input())
+#num = n
+
+#for x in range(0,n):
+#    print(num)
+#    num = num - 1
+
+
+# 5. 별 N 개를 찍는 문제
+
+#N = int(input())
+#n = 1
+
+#for x in range(N):
+#   print('*'*n)
+#   n = n + 1
+
+# 6. 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램
+
+#a,b = input('').split()
+#a = int(a)
+#b = int(b)
+#print(a+b)
+
+
+
