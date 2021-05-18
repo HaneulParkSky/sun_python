5cc7bffe38fa6e39356a7e5ea6d2614bb714a1b8
Sky <45191981+HaneulParkSky@users.noreply.github.com>
2021-05-18 15:55:58 +0900

    Add files via upload

---
+++ ex_PITCA_01.py
@@ -0,0 +1,13 @@
+
+
+# 1. 기본 입출력
+
+print("안녕하세요, 저는 pitca bot입니다.")
+print()
+
+name = input("당신의 이름을 알려주세요:")
+age = int(input("{}님, 당신의 나이는?:".format(name)))
+
+print()
+print("환영합니다. {}님!".format(name))
+print("당신은 다음해에 {}살이 되는군요!".format(age + 1))
