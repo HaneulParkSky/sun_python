245029503e974e21357b3e1bfccc2f93039f0a82
Sky <45191981+HaneulParkSky@users.noreply.github.com>
2021-05-25 22:11:11 +0900

    Add files via upload

---
+++ jumptopython_01.py
@@ -0,0 +1,26 @@
+
+
+# 점프투파이썬
+# https://wikidocs.net/32830
+
+# 001
+
+def print_coin():
+    print("비트코인")
+
+# 002
+
+print_coin()
+
+# 003
+
+for x in range(100):
+    print_coin()
+
+# 004
+
+def print_coins():
+    for x in range(100):
+        print("비트코인")
+
+
